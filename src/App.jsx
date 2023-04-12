a21ab4ef4667fcabaf8fe02ff6b94b681d53bdee
akinyiliz <lizzieke84@gmail.com>
2023-04-12 17:24:41 +0300

    Removed unnecessary lines of code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <div className="AngaAPP">
-      
-    </div>
-  )
+	return (
+		<div className="AngaAPP">
+			<h1>AngaApp</h1>
+		</div>
+	);
 }
 
-export default App
+export default App;
