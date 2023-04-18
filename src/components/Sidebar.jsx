a98117f888206be146f71e2f3ecd747a3cc57bf3
akinyiliz <lizzieke84@gmail.com>
2023-04-18 22:48:23 +0300

    Created Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function Sidebar() {
+	return (
+		<div className="w-1/5 h-full right-0 top-0 bottom-0 ">
+			<div className="days-container text-white h-[70vh] bg-[#302579]">
+				<div className="day h-16">
+					<h2>Sunday</h2>
+				</div>
+				<div className="day h-16">
+					<h2>Monday</h2>
+				</div>
+				<div className="day h-16">
+					<h2>Tuesday</h2>
+				</div>
+				<div className="day h-16">
+					<h2>Wednesday</h2>
+				</div>
+				<div className="day h-16">
+					<h2>Thursday</h2>
+				</div>
+				<div className="day h-16">
+					<h2>Friday</h2>
+				</div>
+				<div className="day h-16">
+					<h2>Saturday</h2>
+				</div>
+			</div>
+			<div className="h-[10rem] mt-4 bg-[#302579]">
+				<h1>SUNRISE & SUNSET</h1>
+			</div>
+		</div>
+	);
+}
