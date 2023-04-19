89b43222aa211f986269af4c1850ee79dd08613e
Rickyflame <kimumuricky@gmail.com>
2023-04-19 15:35:18 +0300

    added APIs

---
+++ src/components/api.jsx
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cd25366237msh428164d68d7ebc7p1c368cjsnbcf0a436fdb0',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
