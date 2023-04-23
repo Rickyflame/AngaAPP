5b27e14b5814efcf63815bd7e6bac932341d7d0e
akinyiliz <lizzieke84@gmail.com>
2023-04-23 14:55:25 +0300

    Fetched current weather data & forecast data asynchronously using promise

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,34 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-export default function Navbar({ setWeatherData }) {
-	const [data, setData] = useState({});
+export default function Navbar({ setWeatherData, setForecastData }) {
+	const [currentWeather, setcurrentWeather] = useState({});
 	const [location, setLocation] = useState("");
 
 	const WEATHER_API_URL = import.meta.env.VITE_WEATHER_API_URL;
 	const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
-	const url = `${WEATHER_API_URL}/current?city=${location}&key=${WEATHER_API_KEY}`;
-
 	// Search weather of a city
 	const searchLocation = (event) => {
 		if (event.key == "Enter") {
-			fetch(url)
-				.then((response) => response.json())
-				.then((weatherData) => {
-					setWeatherData(weatherData);
-					setData(weatherData);
-					console.log(weatherData);
+			// Fetching current weather & forecast of searched location
+			const searchedLocationCurrentWeatherUrl = `${WEATHER_API_URL}/current?city=${location}&key=${WEATHER_API_KEY}`;
+			const searchedLocationForecastUrl = `${WEATHER_API_URL}/forecast/daily?city=${location}&key=${WEATHER_API_KEY}`;
+
+			Promise.all([
+				fetch(searchedLocationCurrentWeatherUrl),
+				fetch(searchedLocationForecastUrl),
+			])
+				.then((responses) =>
+					Promise.all(responses.map((response) => response.json()))
+				)
+				.then((data) => {
+					const searchedLocationWeatherResponse = data[0];
+					const searchedLocationForecastResponce = data[1];
+
+					setWeatherData(searchedLocationWeatherResponse);
+					setcurrentWeather(searchedLocationWeatherResponse);
+					setForecastData(searchedLocationForecastResponce);
 				});
 			setLocation("");
 		}
@@ -34,15 +44,32 @@ export default function Navbar({ setWeatherData }) {
 				(position) => {
 					const latitude = position.coords.latitude;
 					const longitude = position.coords.longitude;
-					// Use latitude and longitude to fetch weather data
-					fetch(
-						`${WEATHER_API_URL}/current?lat=${latitude}&lon=${longitude}&key=${WEATHER_API_KEY}`
-					)
-						.then((responce) => responce.json())
-						.then((weatherData) => {
-							setWeatherData(weatherData);
-							setData(weatherData);
-						});
+
+					// Fetching current weather & forecast using latitude and longitude(current location)
+					const currentLocationWeatherUrl = `${WEATHER_API_URL}/current?lat=${latitude}&lon=${longitude}&key=${WEATHER_API_KEY}`;
+					const currentLocationForecastUrl = `${WEATHER_API_URL}/forecast/daily?lat=${latitude}&lon=${longitude}&key=${WEATHER_API_KEY}`;
+
+					Promise.all([
+						fetch(currentLocationWeatherUrl),
+						fetch(currentLocationForecastUrl),
+					])
+						.then((responses) =>
+							Promise.all(responses.map((response) => response.json()))
+						)
+						.then((data) => {
+							const currentLocationWeatherResponse = data[0];
+							const currentLocationforecastResponse = data[1];
+
+							setcurrentWeather(currentLocationWeatherResponse);
+							setForecastData(currentLocationforecastResponse);
+							setWeatherData(currentLocationWeatherResponse);
+
+							console.log(
+								currentLocationWeatherResponse,
+								currentLocationforecastResponse
+							);
+						})
+						.catch((error) => console.error(error));
 				},
 				(error) => {
 					console.error("Error getting location:", error);
@@ -68,9 +95,10 @@ export default function Navbar({ setWeatherData }) {
 				/>
 			</div>
 			<div className="location-container">
-				{data.data ? (
+				{currentWeather.data ? (
 					<p>
-						{data.data[0].city_name}, {data.data[0].country_code}
+						{currentWeather.data[0].city_name},
+						{currentWeather.data[0].country_code}
 					</p>
 				) : null}
 			</div>
