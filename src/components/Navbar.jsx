7f7a862d7458c4c6475ec0614095aa6added0bba
akinyiliz <lizzieke84@gmail.com>
2023-04-26 15:11:19 +0300

    Fetched hourly forecast for current & searched location

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,23 +4,34 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-function Navbar({ setWeatherData, setForecastData }) {
+function Navbar({ setWeatherData, setForecastData, setHourlyData }) {
 	const [currentWeather, setcurrentWeather] = useState({});
 	const [location, setLocation] = useState("");
 
 	const WEATHER_API_URL = import.meta.env.VITE_WEATHER_API_URL;
 	const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
+	// Get current date
+	const currentDate = new Date();
+	// Get the date for the next day
+	const nextDate = new Date();
+	nextDate.setDate(currentDate.getDate() + 1);
+	// format the dates as strings in the desired format
+	const startDate = currentDate.toISOString().substring(0, 10);
+	const endDate = nextDate.toISOString().substring(0, 10);
+
 	// Search weather of a city
 	const searchLocation = (event) => {
 		if (event.key == "Enter") {
-			// Fetching current weather & forecast of searched location
+			// Fetching current weather, hourly forecast & daily forecast of searched location
 			const searchedLocationCurrentWeatherUrl = `${WEATHER_API_URL}/current?city=${location}&key=${WEATHER_API_KEY}`;
 			const searchedLocationForecastUrl = `${WEATHER_API_URL}/forecast/daily?city=${location}&key=${WEATHER_API_KEY}`;
+			const searchedLocationHourlyForecastUrl = `${WEATHER_API_URL}/history/hourly?city=${location}&start_date=${startDate}&end_date=${endDate}&tz=local&key=${WEATHER_API_KEY}`;
 
 			Promise.all([
 				fetch(searchedLocationCurrentWeatherUrl),
 				fetch(searchedLocationForecastUrl),
+				fetch(searchedLocationHourlyForecastUrl),
 			])
 				.then((responses) =>
 					Promise.all(responses.map((response) => response.json()))
@@ -28,10 +39,12 @@ function Navbar({ setWeatherData, setForecastData }) {
 				.then((data) => {
 					const searchedLocationWeatherResponse = data[0];
 					const searchedLocationForecastResponce = data[1];
+					const searchedLocationHourlyForecastResponse = data[2];
 
 					setWeatherData(searchedLocationWeatherResponse);
 					setcurrentWeather(searchedLocationWeatherResponse);
 					setForecastData(searchedLocationForecastResponce);
+					setHourlyData(searchedLocationHourlyForecastResponse);
 				});
 			setLocation("");
 		}
@@ -45,13 +58,15 @@ function Navbar({ setWeatherData, setForecastData }) {
 					const latitude = position.coords.latitude;
 					const longitude = position.coords.longitude;
 
-					// Fetching current weather & forecast using latitude and longitude(current location)
+					// Fetching current weather, hourly forecast & daily forecast using latitude and longitude(current location)
 					const currentLocationWeatherUrl = `${WEATHER_API_URL}/current?lat=${latitude}&lon=${longitude}&key=${WEATHER_API_KEY}`;
 					const currentLocationForecastUrl = `${WEATHER_API_URL}/forecast/daily?lat=${latitude}&lon=${longitude}&key=${WEATHER_API_KEY}`;
+					const currentLocationHourlyForecastUrl = `${WEATHER_API_URL}/history/hourly?lat=${latitude}&lon=${longitude}&start_date=${startDate}&end_date=${endDate}&tz=local&key=${WEATHER_API_KEY}`;
 
 					Promise.all([
 						fetch(currentLocationWeatherUrl),
 						fetch(currentLocationForecastUrl),
+						fetch(currentLocationHourlyForecastUrl),
 					])
 						.then((responses) =>
 							Promise.all(responses.map((response) => response.json()))
@@ -59,14 +74,17 @@ function Navbar({ setWeatherData, setForecastData }) {
 						.then((data) => {
 							const currentLocationWeatherResponse = data[0];
 							const currentLocationforecastResponse = data[1];
+							const currentLocationHourlyForecastResponse = data[2];
 
 							setcurrentWeather(currentLocationWeatherResponse);
 							setForecastData(currentLocationforecastResponse);
 							setWeatherData(currentLocationWeatherResponse);
+							setHourlyData(currentLocationHourlyForecastResponse);
 
 							console.log(
 								currentLocationWeatherResponse,
-								currentLocationforecastResponse
+								currentLocationforecastResponse,
+								currentLocationHourlyForecastResponse
 							);
 						})
 						.catch((error) => console.error(error));
