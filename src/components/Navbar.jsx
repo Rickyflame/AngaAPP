69c4e9dd209905d554869a37349b1a05c5c3f9e3
Rickyflame <kimumuricky@gmail.com>
2023-04-27 00:09:25 +0300

    current weather and forecast

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,132 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../App.css";
-import Search from "./Search";
-import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
-import { data } from "autoprefixer";
+import * as dotenv from "dotenv";
+import logo from "../assets/logo.png";
 
-export default function Navbar() {
+dotenv.config();
 
-	const [currentWeather, setCurrentWeather] = useState(null);
-	const [forecast, setForecast] = useState(null);
-/* the const below is here because it calls the search data   */
+function Navbar({ setWeatherData, setForecastData, setHourlyData }) {
+	const [currentWeather, setcurrentWeather] = useState({});
+	const [location, setLocation] = useState("");
 
-	const handleOnSearchChange = (searchData) => {
-		const [lat, lon] = searchData.value.split(" ");
+	const WEATHER_API_URL = import.meta.env.VITE_WEATHER_API_URL;
+	const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
-		const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+	// Get current date
+	const currentDate = new Date();
+	// Get the date for the next day
+	const nextDate = new Date();
+	nextDate.setDate(currentDate.getDate() + 1);
+	// format the dates as strings in the desired format
+	const startDate = currentDate.toISOString().substring(0, 10);
+	const endDate = nextDate.toISOString().substring(0, 10);
 
-		const forecastFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+	// Search weather of a city
+	const searchLocation = (event) => {
+		if (event.key == "Enter") {
+			// Fetching current weather, hourly forecast & daily forecast of searched location
+			const searchedLocationCurrentWeatherUrl = `${WEATHER_API_URL}/current?city=${location}&key=${WEATHER_API_KEY}`;
+			const searchedLocationForecastUrl = `${WEATHER_API_URL}/forecast/daily?city=${location}&key=${WEATHER_API_KEY}`;
+			const searchedLocationHourlyForecastUrl = `${WEATHER_API_URL}/history/hourly?city=${location}&start_date=${startDate}&end_date=${endDate}&tz=local&key=${WEATHER_API_KEY}`;
 
-		Promise.all([currentWeatherFetch, forecastFetch])
-			.then(async (response) => {
-				const weatherResponse = await response[0].json();
-				const forecastResponse = await response[1].json();
-				/* update current response */
+			Promise.all([
+				fetch(searchedLocationCurrentWeatherUrl),
+				fetch(searchedLocationForecastUrl),
+				fetch(searchedLocationHourlyForecastUrl),
+			])
+				.then((responses) =>
+					Promise.all(responses.map((response) => response.json()))
+				)
+				.then((data) => {
+					const searchedLocationWeatherResponse = data[0];
+					const searchedLocationForecastResponce = data[1];
+					const searchedLocationHourlyForecastResponse = data[2];
 
-				/* this add the cityname and countrycode for aesthetics */
-				setCurrentWeather({ city: searchData.Label ,...weatherResponse});
-				setForecast({ city: searchData.Label ,...forecastResponse});
-			})
-			.catch((err) => console.log(err));
-	}
+					setWeatherData(searchedLocationWeatherResponse);
+					setcurrentWeather(searchedLocationWeatherResponse);
+					setForecastData(searchedLocationForecastResponce);
+					setHourlyData(searchedLocationHourlyForecastResponse);
+				});
+			setLocation("");
+		}
+	};
 
-	console.log(currentWeather);
-	console.log(forecast);
+	useEffect(() => {
+		// Check if browser has geolocation
+		if ("geolocation" in navigator) {
+			navigator.geolocation.getCurrentPosition(
+				(position) => {
+					const latitude = position.coords.latitude;
+					const longitude = position.coords.longitude;
+
+					// Fetching current weather, hourly forecast & daily forecast using latitude and longitude(current location)
+					const currentLocationWeatherUrl = `${WEATHER_API_URL}/current?lat=${latitude}&lon=${longitude}&key=${WEATHER_API_KEY}`;
+					const currentLocationForecastUrl = `${WEATHER_API_URL}/forecast/daily?lat=${latitude}&lon=${longitude}&key=${WEATHER_API_KEY}`;
+					const currentLocationHourlyForecastUrl = `${WEATHER_API_URL}/history/hourly?lat=${latitude}&lon=${longitude}&start_date=${startDate}&end_date=${endDate}&tz=local&key=${WEATHER_API_KEY}`;
+
+					Promise.all([
+						fetch(currentLocationWeatherUrl),
+						fetch(currentLocationForecastUrl),
+						fetch(currentLocationHourlyForecastUrl),
+					])
+						.then((responses) =>
+							Promise.all(responses.map((response) => response.json()))
+						)
+						.then((data) => {
+							const currentLocationWeatherResponse = data[0];
+							const currentLocationforecastResponse = data[1];
+							const currentLocationHourlyForecastResponse = data[2];
+
+							setcurrentWeather(currentLocationWeatherResponse);
+							setForecastData(currentLocationforecastResponse);
+							setWeatherData(currentLocationWeatherResponse);
+							setHourlyData(currentLocationHourlyForecastResponse);
+
+							console.log(
+								currentLocationWeatherResponse,
+								currentLocationforecastResponse,
+								currentLocationHourlyForecastResponse
+							);
+						})
+						.catch((error) => console.error(error));
+				},
+				(error) => {
+					console.error("Error getting location:", error);
+				}
+			);
+		} else {
+			console.error("Geolocation API is not available in this browser.");
+		}
+	}, []);
 
 	return (
 		<div className="navbar">
 			<div className="">
-				<h1 className="logo">AngaAPP</h1>
+				<img
+					src={logo}
+					alt="logo"
+					className="logo"
+				/>
 			</div>
-			<Search onSearchChange={handleOnSearchChange}/>
-			{currentWeather && <currentWeather data={currentWeather} />}
-			<div className="location-container bg-slate-600">
-				<h1 className="location">location</h1>
+			<div className="search-bar">
+				<input
+					type="text"
+					placeholder="Search for city"
+					value={location}
+					onChange={(event) => setLocation(event.target.value)}
+					onKeyPress={searchLocation}
+				/>
+			</div>
+			<div className="location-container">
+				{currentWeather.data ? (
+					<p>
+						{currentWeather.data[0].city_name},
+						{currentWeather.data[0].country_code}
+					</p>
+				) : null}
 			</div>
 		</div>
 	);
 }
+
+export default Navbar;
