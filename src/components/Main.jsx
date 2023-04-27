c30c7658ba863b13a35f28e43c58fb925d447ffe
akinyiliz <lizzieke84@gmail.com>
2023-04-27 10:45:27 +0300

    Added responsiveness

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -60,45 +60,51 @@ function Main() {
 	const eveningTemperature = getAverageTemperature(eveningData);
 
 	return (
-		<div className="flex-grow h-full mx-8">
+		<div className=" h-full mx-1 md:mx-4 lg:mx-8">
 			{/* Navbar */}
 			<Navbar
 				setWeatherData={setData}
 				setForecastData={setForecast}
 				setHourlyData={setHourlyData}
 			/>
-			<div className="flex">
+			<div className="flex flex-col md:flex md:flex-row lg:flex lg:flex-row ">
 				{/* Left side */}
-				<div className="w-[80%]">
+				<div className="w-[100%] md:w-[80%] lg:w-[80%] ">
 					<div className="main-element">
-						<div className="flex gap-10">
-							<div className="flex flex-col ml-4">
+						<div className="flex flex-col md:flex  md:flex-row lg:flex lg:flex-row lg:gap-10">
+							<div className="flex flex-col md:mt-4 md:flex-col lg:flex-col ml-4">
 								{data.data ? (
 									<h1 className="temperature">
 										{data.data[0].temp.toFixed()}°C
 									</h1>
 								) : null}
+								<div className="flex flex-row items-center md:flex-col lg:flex lg:flex-col">
+									<div>
+										<p className="date">{formattedDate}</p>
+										{data.data ? (
+											<p className="description">
+												{data.data[0].weather.description}
+											</p>
+										) : null}
+									</div>
 
-								<p className="date">{formattedDate}</p>
-								{data.data ? (
-									<p className="description">
-										{data.data[0].weather.description}
-									</p>
-								) : null}
-								{data.data ? (
-									<img
-										src={`icons/${data.data[0].weather.icon}.png`}
-										alt="weather"
-										className="weather-icon"
-									/>
-								) : null}
+									{data.data ? (
+										<img
+											src={`icons/${data.data[0].weather.icon}.png`}
+											alt="weather"
+											className="weather-icon"
+										/>
+									) : null}
+								</div>
 							</div>
 							<div className="parts-of-day-container">
 								<div className="parts-of-day morning">
-									<p className="mx-4">Morning</p>
-									<div className="flex items-center justify-between mx-4">
+									<p className="mx-1 lg:mx-4">Morning</p>
+									<div className="flex items-center justify-between mx-1 lg:mx-4">
 										<div>
-											<p className="text-[50px]">{morningTemperature}°</p>
+											<p className="text-[30px] lg:text-[50px]">
+												{morningTemperature}°
+											</p>
 											{hourlyData.data ? (
 												<p className="part-of-day-desc">
 													{hourlyData.data[12].weather.description}
@@ -115,11 +121,13 @@ function Main() {
 									</div>
 								</div>
 								<div className="parts-of-day midday">
-									<p className="mx-4">Midday</p>
+									<p className="mx-1 lg:mx-4">Midday</p>
 
-									<div className="flex items-center justify-between mx-4">
+									<div className="flex items-center justify-between mx-1 lg:mx-4">
 										<div>
-											<p className="text-[50px]">{middayTemperature}°</p>
+											<p className="text-[30px] lg:text-[50px]">
+												{middayTemperature}°
+											</p>
 											{hourlyData.data ? (
 												<p className="part-of-day-desc">
 													{hourlyData.data[12].weather.description}
@@ -137,10 +145,12 @@ function Main() {
 									</div>
 								</div>
 								<div className="parts-of-day evening">
-									<p className="mx-4">Evening</p>
-									<div className="flex items-center justify-between mx-4">
+									<p className="mx-1 lg:mx-4">Evening</p>
+									<div className="flex items-center justify-between mx-1 lg:mx-4">
 										<div>
-											<p className="text-[50px]">{eveningTemperature}°</p>
+											<p className="text-[30px] lg:text-[50px]">
+												{eveningTemperature}°
+											</p>
 											{hourlyData.data ? (
 												<p className="part-of-day-desc">
 													{hourlyData.data[20].weather.description}
@@ -159,7 +169,7 @@ function Main() {
 							</div>
 						</div>
 					</div>
-					<div className="grid grid-cols-3 gap-10 mt-4 w-[95%]">
+					<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3 md:gap-4 md:w-[95%] lg:gap-10 mt-4 w-[100%] lg:w-[95%]">
 						<div className="elements">
 							<div className="elements-content">
 								<p className="elements-title">Feels like</p>
@@ -230,7 +240,7 @@ function Main() {
 							<img
 								src={Cloud}
 								alt="weather"
-								className="h-[80%]"
+								className="h-[50%] lg:h-[80%]"
 							/>
 						</div>
 						<div className="elements">
